118a67cfded449fbc88c464fb9a62f057815ac2b
Zaid Liaqat <zaidliaqat999@gmail.com>
2026-05-27 19:00:20 +0500

    feat: add new UI components and theme provider
    - Updated package.json to include new dependencies for UI components.
    - Created components.json for component configuration.
    - Implemented ThemeProvider component for theme management.
    - Added Button component with variants and styles using class-variance-authority.
    - Introduced utility function for merging class names with Tailwind CSS.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import { Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -11,9 +13,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
     >
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        {/* header */}
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="min-h-screen">{children}</main>
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p>Made with ❤️ by Zaid</p>
+            </div>
+          </footer>
+        </ThemeProvider>
+
+      </body>
     </html>
   );
 }
